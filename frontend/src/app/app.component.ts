f70ea99b6ccba0f30c8d11414c6ac18a1f87622e
Vanshi shah <vanshi@1405shah@gmail.com>
2025-04-22 18:31:05 +0530

    Frontend:- - made basic UI for the column selection and the role dropdown.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,37 +1,91 @@
-import { Component, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-import {RouterModule} from '@angular/router';
+// import { Component, inject, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { CommonModule } from '@angular/common';
+// import {RouterModule} from '@angular/router';
+// import { TableModule } from 'primeng/table';
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [CommonModule, RouterModule, TableModule],
+//   templateUrl: 'app.component.html',
+// })
+// export class AppComponent implements OnInit {
+//   private http = inject(HttpClient);
+
+//   response: string | null = null;
+//   error: string | null = null;
+//   isLoading = false;
+
+//   products=[];
+
+//   constructor(private productService: ProductService) {}
+
+//   ngOnInit() {
+//     this.productService.getProductsMini().then((data) => {
+//         this.products = data;
+//     });
+// }
+
+//   // callApi() {
+//   //   this.isLoading = true;
+//   //   this.error = null;
+//   //   this.response = null;
+
+//   //   this.http.get<any>('http://localhost:8000/api/demo/').subscribe({
+//   //     next: (res) => {
+//   //       this.response = res.message;
+//   //     },
+//   //     error: (err) => {
+//   //       this.error = err.message;
+//   //       console.error('API Error:', err);
+//   //     },
+//   //     complete: () => {
+//   //       this.isLoading = false;
+//   //     }
+//   //   });
+//   // }
+// }
+
+
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- Add this
+import { Product } from '../domain/product';
+import { ProductService } from '../service/productservice';
+import { RouterModule } from '@angular/router';
+import { TableModule } from 'primeng/table';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { AutoCompleteCompleteEvent } from 'primeng/autocomplete';
+import { FormsModule } from '@angular/forms'; // ⬅️ Add this import
+
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
   templateUrl: 'app.component.html',
+  standalone: true,
+  imports: [FormsModule, CommonModule, TableModule, RouterModule, AutoCompleteModule], // <-- Add CommonModule here
+  providers: [ProductService]
 })
-export class AppComponent {
-  private http = inject(HttpClient);
 
-  response: string | null = null;
-  error: string | null = null;
-  isLoading = false;
+export class AppComponent implements OnInit {
+  roles:any= [] ;
 
-  callApi() {
-    this.isLoading = true;
-    this.error = null;
-    this.response = null;
+  roleValue='';
+  products!: Product[];
+  count = Array.from({ length: 10 });
 
-    this.http.get<any>('http://localhost:8000/api/demo/').subscribe({
-      next: (res) => {
-        this.response = res.message;
-      },
-      error: (err) => {
-        this.error = err.message;
-        console.error('API Error:', err);
-      },
-      complete: () => {
-        this.isLoading = false;
-      }
+  constructor(private productService: ProductService) {}
+
+  search(event: any) {
+    let _roles = [...Array(10).keys()];
+
+    this.roles = event.query ? [...Array(10).keys()].map((role) => event.query + '-' + role) : _roles;
+    }
+
+
+  ngOnInit() {
+    this.productService.getProductsMini().then((data) => {
+      this.products = data;
     });
   }
 }
--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import { provideAnimations } from '@angular/platform-browser/animations';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
-    // Add other providers here as needed
+    provideAnimations(),
+    providePrimeNG({
+      theme: {
+          preset: Aura
+      }
+  })
   ]
 };
